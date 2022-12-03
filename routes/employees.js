1acc6f53577437d77d5868dbbfca60a73fed3135
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-12-03 14:34:41 -0500

    Refractored express, sequelize code and routes

--- routes/employees.js
+++ routes/employees.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database/db')
-const Employee = require('../database/models/Employee');
+const { Task, Employee } = require('../database/models');
 
-router.get('/', (req, res) =>
-    Employee.findAll()
-        .then(employees => {
-            console.log(employees);
-            res.sendStatus(200);
-        })
-        .catch(err => console.log(err)));
+// helper function so we don't need to wrap our
+// handler functions in try-catch blocks;
+// the async handler will catch any errors and pass
+// them to the error-handling middleware (defined in app.js)
+const ash = require('express-async-handler');
+
+/** GET ALL EMPLOYEES */
+router.get('/', ash(async(req, res) => {
+  let employees = await Employee.findAll({include: [Task]});
+  res.status(200).json(employees);
+}));
 
 module.exports = router
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database/db')
-const Task = require('../database/models/Task');
+const { Task, Employee } = require('../database/models');
 
-router.get('/', (req, res) =>
-    Task.findAll()
-        .then(tasks => {
-            console.log(tasks);
-            res.sendStatus(200);
-        })
-        .catch(err => console.log(err)));
+// helper function so we don't need to wrap our
+// handler functions in try-catch blocks;
+// the async handler will catch any errors and pass
+// them to the error-handling middleware (defined in app.js)
+const ash = require('express-async-handler');
 
-module.exports = router
+/** GET ALL TASKS: express-async-handler (ash) */
+// automatically catches any error and sends to middleware
+// same as using try/catch and calling next(error)
+router.get('/', ash(async (req, res) => {
+  //{include: [Employee]}
+  let tasks = await Task.findAll();
+  res.status(200).json(tasks);
+}));
+
+module.exports = router;
